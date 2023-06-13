8f4d448143cf3f20bbe5d95d0bd8143df66f9514
federico Chandia <fedelchandia@gmail.com>
2023-06-13 12:58:14 -0300

    feat: imagen

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express();
 const path = require('path');
 const PORT = 3030;
 
+/* configuraciones*/
+app.use(express.static('public'))
+
 /* Rutas */
 app.get('/home',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
 app.get('/about',(req,res) => res.sendFile(path.join(__dirname,'views','about.html')));
 app.get('/music',(req,res) => res.sendFile(path.join(__dirname,'views','music.html')))
-app.get('/*',(req,res) => res.sendfile(path.join(__dirname,'views',)))
+app.get('/*',(req,res) => res.sendFile(path.join(__dirname,'views',)))
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' +PORT));
